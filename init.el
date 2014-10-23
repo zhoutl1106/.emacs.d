67959dc83678e2ac3d8f2f7fed699db782d74666
zhoutl1106 <zhoutl1106@126.com>
2014-10-23 08:48:47 +0800

    add smex.el

--- init.el
+++ init.el
@@ -44,3 +44,11 @@
 (tool-bar-mode -1)
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
+
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+                  ; when Smex is auto-initialized on its first run.
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
